501c2f0b2f8a55004e03bb4bce88d99fa079f6b6
Meryem Ebru Uyar <eebru.uyarr@gmail.com>
2025-11-25 13:41:56 +0300

    Add files via upload

---
+++ methods_practice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static int intMethod(int x,int y)
+        {
+            return x * y;
+        }
+        static double doubleMethod(double x, double y)
+        {
+            return x +y;
+        }
+
+        static void Main(string[] args)
+        {
+            int myNum = intMethod(4, 6);
+            double myDouble = doubleMethod(3.6, 2.4);
+            Console.WriteLine(myNum);
+            Console.WriteLine(myDouble);
+
+        }
+    }
+}
